0541b8ba5711caa15a5a005bc7f84447d50a58a2
Johanna Moritz <johannamoritz990@hotmail.com>
2018-11-10 21:06:27 +0000

    automatically backed up by learn

---
+++ hello_world.rb
@@ -0,0 +1,45 @@
+def hello (name)
+  # Interpoilation means we can put the variable in our string but still tell Ruby that its a variable and not a literal string 
+  puts "Hello #{name}"
+end
+# when we run this method we want it to print "Hello name" the variable name whatever that is. that is entered.
+
+
+
+
+
+def add (arg1, arg2)
+ return arg1 + arg2
+  end
+  
+  def add (arg1, arg2)
+ return arg1 + arg2
+  end
+  
+  def add (arg1, arg2)
+ return arg1 + arg2
+  end
+  
+  def add (arg1, arg2)
+ return arg1 + arg2
+  end
+  
+
+# add 1 + 1 and 2 as the result
+# arg1 = arg1
+# arg2 = arg2
+add(1, 1)
+
+
+
+
+def add_string()
+  
+  
+end 
+  
+
+  
+  
+  
+
